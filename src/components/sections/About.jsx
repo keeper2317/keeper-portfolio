be9b44d172330fbd7b9e5160cf22bbf3a6db9527
keeper2317 <keeperjhoilfloresca@iskolarngbayan.pup.edu.ph>
2025-07-23 21:10:14 +0800

    Updated projects section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -76,7 +76,7 @@ export const About = () => {
                         </div>
 
                         <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                            <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                            <h3 className="text-xl font-bold mb-4"> Backend</h3>
                             <div className="flex flex-wrap gap-2">
                                 {backendSkills.map((tech, key) => (
                                     <span
